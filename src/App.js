c219fc65a0fe84e2aacffeee62fc19d71fa0e087
ZulfikarRofi <74011218+ZulfikarRofi@users.noreply.github.com>
2022-12-20 05:36:46 +0700

    new update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blank from './pages/Blank';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    // <h1 className="text-3xl font-bold underline">
+    //   Hello world!
+    // </h1>
+    <Routes>
+      <Route exact path='/' element={<Blank />} />
+    </Routes>
   );
 }
 
